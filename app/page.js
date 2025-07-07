edf6b49f3ae171021c8d055ef60cdb5214033854
Thomas collins <thjcollins42@gmail.com>
2025-07-07 21:08:03 +0100

    Singign test  -  still not wokring

--- app/page.js
+++ app/page.js
@@ -197,7 +197,7 @@ export default function Home() {
                 lineHeight: 1.2,
               }}
             >
-              Version 0.0.9
+              Version 0.1.0
             </span>
           </div>
         </div>
